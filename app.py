a383188b639db1d11c3a22eda93fb51b46156373
tiffanyyan3 <tiffyan@uw.edu>
2023-03-13 16:53:29 -0700

    <set up html>

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/results", methods=["GET","POST"])
+def results():
+    if request.method == "POST":
+        place = request.form["place"]
+        max_results = int(request.form["max_results"])
+        radius = float(request.form["radius"])
+        sort = "sort" in request.form
+        results = functions.wikipedia_locationsearch(place,max_results=max_results,radius=radius,sort=sort)
+        #return "Results that are close to {}".format(place)
+        return render_template("results.html", results=results, place=place)
+    else:
+        return "Wrong HTTP method", 400
